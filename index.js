099b07c0fa0b3221b6e7351471ead8c6f4535cba
bitalyk <152621224+bitalyk@users.noreply.github.com>
2024-08-22 00:58:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
+app.get("/s", (req, res) => {
   const address = req.query.address;  // Extract the address from query parameters
   runPuppeteerScript(address, res);  // Pass the address to scrapeLogic
 });
